3c4ad3b3cdc5d3ad4ff62520343be3ae42e122d6
Tom Ribbens <tom@tomribbens.be>
2012-03-11 17:52:59 +0100

    Create init.sh to quickly set up everything

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Link files
+ln -is ~/.shellcustomization/authorized_keys ~/.ssh/
+ln -is ~/.shellcustomization/.bashrc ~/
+ln -is ~/.shellcustomization/.bash_profile ~/
+ln -is ~/.shellcustomization/.screenrc ~/
